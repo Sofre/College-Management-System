0ab28577af34f22aa2541335d19fe7df04dd260e
Duki <dukisofronievski@gmail.com>
2025-05-09 01:28:05 +0200

    FLOWRIDER -- commmited database change

--- collegedata.sql
+++ collegedata.sql
@@ -1,12 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 5.0.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Oct 18, 2020 at 08:47 AM
--- Server version: 10.4.11-MariaDB
--- PHP Version: 7.4.3
-
+------ FLOWRIDERS -------
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
