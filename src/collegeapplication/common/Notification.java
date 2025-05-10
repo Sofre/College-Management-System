d2fb850436135c15bfb67b1b85f6e9ef4a6a9dc7
Duki <dukisofronievski@gmail.com>
2025-05-10 22:18:08 +0200

    commited change

--- src/collegeapplication/common/Notification.java
+++ src/collegeapplication/common/Notification.java
@@ -5,7 +5,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import collegeapplication.course.Cource;
-
+/// note notification scourge
 public class Notification extends Cource
 {
 	
